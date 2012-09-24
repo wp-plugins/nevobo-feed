1c30accde90a66e10b8c26329de6eea0b413b9bb
Masselink <Masselink@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-24 21:38:18 +0000

    ical image fix - for real now
    git-svn-id: https://plugins.svn.wordpress.org/nevobo-feed/trunk@603299 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nevobo-feed.php
+++ nevobo-feed.php
@@ -143,9 +143,8 @@ function get_nevobo($feed,$aantal="20",$sporthal="",$plaats="",$cache="4",$ical=
             }
             $code .= "</tbody></table>";
             if (($nevobo_feedtype==3) && ($ical==1)) {
-
             	$icalfeed = str_replace("format=rss", "format=ical", $feed);
-            	$code .= '<a href="'.$icalfeed.'"><img align="absmiddle" src="/wp-content/plugins/nevobo-feed/images/ical_grey.png"> Voeg het volledige programma toe aan je agenda</a><br /><br />'; 
+            	$code .= '<a href="'.$icalfeed.'"><img align="absmiddle" src="'.get_site_url().'/wp-content/plugins/nevobo-feed/images/ical_grey.png"> Voeg het volledige programma toe aan je agenda</a><br /><br />'; 
 
         }
         }
